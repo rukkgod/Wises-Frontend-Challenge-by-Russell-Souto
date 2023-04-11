58ff2d53d4638b11cb154cf9e675f23e1dae476b
russell <russellxd3@gmail.com>
2023-04-11 04:54:35 -0300

    meu commit

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const emailInput = document.getElementById("email");
 const mensagemInput = document.getElementById("mensagem");
 
 
-function validar() {
+function validar() 
   const nome = nomeInput.value.trim();
   const email = emailInput.value.trim();
   const mensagem = mensagemInput.value.trim();
@@ -14,22 +14,4 @@ function validar() {
     return false;
   }
 
-  if (nome !== '') {
-    nomeInput.parentNode.classList.remove('error');
-  } else {
-    nomeInput.parentNode.classList.add('error');
-  }
-
-  if (email !== '') {
-    emailInput.parentNode.classList.remove('error');
-  } else {
-    emailInput.parentNode.classList.add('error');
-  }
-
-  if (mensagem !== '') {
-    mensagemInput.parentNode.classList.remove('error');
-  } else {
-    mensagemInput.parentNode.classList.add('error');
-  }
-}
-
+  
